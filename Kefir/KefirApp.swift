c2cc576d446d6933524bd974d755ce9d74b3130f
Alex Mazanov <alexandr.mazanov@gmail.com>
2025-06-03 20:18:04 -0400

    Initial Commit

---
+++ Kefir/KefirApp.swift
@@ -0,0 +1,17 @@
+//
+//  KefirApp.swift
+//  Kefir
+//
+//  Created by Alexander Mazanov on 6/3/25.
+//
+
+import SwiftUI
+
+@main
+struct KefirApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KefirTests/KefirTests.swift
@@ -0,0 +1,17 @@
+//
+//  KefirTests.swift
+//  KefirTests
+//
+//  Created by Alexander Mazanov on 6/3/25.
+//
+
+import Testing
+@testable import Kefir
+
+struct KefirTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
